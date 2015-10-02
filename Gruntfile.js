f5cca4b7f9a1f43c7abc040b1229aa4ee19f2b51
Varghese Thomas <varghese.thomas@chillr.in>
2015-10-02 19:10:53 +0530

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          sourceMap: true,
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    connect: {
+      all: {
+        options: {
+          port: 9000,
+          hostname: "0.0.0.0",
+          base: ".",
+          keepalive: false,
+          livereload: true,
+          open: true
+        }
+      }
+    },
+
+    watch: {
+      grunt: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js']
+      },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','connect','watch']);
+}
